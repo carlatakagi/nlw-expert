886ec22f431f2550b6bc31ed96e3dfb0bd245287
carlatakagi <carlatakagi@gmail.com>
2024-02-13 17:07:39 -0400

    feat: add voteOnPoll route and schema

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,8 +2,14 @@ import fastify from "fastify";
 import { createPoll } from "./routes/createPoll";
 import { getPoll } from "./routes/getPoll";
 import { voteOnPoll } from "./routes/voteOnPoll";
+import cookie from "@fastify/cookie";
 
 const app = fastify();
+
+app.register(cookie, {
+    secret: "i-am-a-god", // pode assinar o cookie, é uma forma de garantir que o cookie que o usuário está enviando foi gerado pela aplicacao
+    hook: 'onRequest',
+});
 
 app.register(createPoll)
 app.register(getPoll)
