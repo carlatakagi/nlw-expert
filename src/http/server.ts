7abd00cfc5e19ff947a174c0a154bfe61d0618c7
carlatakagi <carlatakagi@gmail.com>
2024-02-13 16:26:34 -0400

    feat: add getPoll route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,13 @@
 import fastify from "fastify";
 import { createPoll } from "./routes/createPoll";
+import { getPoll } from "./routes/getPoll";
+import { voteOnPoll } from "./routes/voteOnPoll";
 
 const app = fastify();
 
 app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port: 3333 }).then(() => {
     console.log("HTTP server running!")
